de9d67e0685c3b94f617793b24e23466f7866671
noelunaa <95946577+noelunaa@users.noreply.github.com>
2022-08-09 07:44:01 -0700

    Add files via upload

---
+++ ESP32CAM_flash.ino
@@ -0,0 +1,27 @@
+//ESP32CAM flash
+//Por: Hugo Vargas
+//24 de mayo de 2021
+//Creditos Moayom
+//
+//Este es el programa básico para controlar el
+//LED Flash del ESP32CAM. El led flash parpadea
+//cada 500 ms.
+
+
+//Definición de pines
+#define LAMP 4 //Se selecciona el pin donde se encuentra el led flash
+
+//Inicialización del programa
+void setup() //Estructura principal
+{
+  pinMode(LAMP, OUTPUT); //Configuración de pines. 2 parámetros, pin a configurar y estado (INPUT,INPUT_PULLUP,OUTPUT)
+} //Fin de void setup ()
+
+//Cuerpo del programa
+void loop() //Función bucle
+{
+  digitalWrite (FLASH, HIGH); //Encender led. 2 parámetros, pin a controlar, estado (0,LOW,FALSE,1,HIGH,TRUE)
+  delay (500); //Espera de 2 segundos
+  digitalWrite (FLASH, LOW); //Apagar led
+  delay (500); //Espera de 2 segundos
+} //Fin de void loop()
